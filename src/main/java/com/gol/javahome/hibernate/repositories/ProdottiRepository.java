2db6620198d1116e9313225a8552c3ec7f1bb77f
Davide-00 <davideboscolo99@gmail.com>
2024-06-28 16:04:10 +0200

    Modificato un metodo in  ProdottiRepository e aggiunto Prodotti-Service

--- src/main/java/com/gol/javahome/hibernate/repositories/ProdottiRepository.java
+++ src/main/java/com/gol/javahome/hibernate/repositories/ProdottiRepository.java
@@ -9,7 +9,6 @@ import java.util.List;
 @Repository
 public interface ProdottiRepository extends JpaRepository<Prodotto, Integer>{
 
-    List<Prodotto> findByIdProdotto(Integer idProdotto);
     List<Prodotto> findByNome(String nome);
     List<Prodotto> findByCategoria(String categoria);
     List<Prodotto> findByColore(String colore);
