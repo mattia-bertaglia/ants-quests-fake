eae220866c2f5692505f06a750cf585b775fb26e
Davide-00 <davideboscolo99@gmail.com>
2024-06-28 15:41:52 +0200

    Aggiunta entity Prodotto

---
+++ src/main/java/com/gol/javahome/hibernate/entities/Prodotto.java
@@ -0,0 +1,30 @@
+package com.gol.javahome.hibernate.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "prodotti")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class Prodotto extends GenericEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idProdotto;
+    private String nome; 
+    private String categoria;
+    private double prezzo; 
+    private int quantita;
+    private String colore;
+    
+}
