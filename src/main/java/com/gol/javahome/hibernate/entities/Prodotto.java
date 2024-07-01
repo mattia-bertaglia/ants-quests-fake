a8ec32c4c0d76b485a65e34ce3de136bb54f597a
Sam89leu <samueltrecca@gmail.com>
2024-07-01 11:57:16 +0200

    fix import

--- src/main/java/com/gol/javahome/hibernate/entities/Prodotto.java
+++ src/main/java/com/gol/javahome/hibernate/entities/Prodotto.java
@@ -3,12 +3,9 @@ package com.gol.javahome.hibernate.entities;
 import java.util.List;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
--- src/main/java/com/gol/javahome/hibernate/entities/Utente.java
+++ src/main/java/com/gol/javahome/hibernate/entities/Utente.java
@@ -5,11 +5,9 @@ import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
