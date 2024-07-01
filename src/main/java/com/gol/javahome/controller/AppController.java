7307a912a16a9b7a52fc8b4522a40790ec7d00bd
ro-ta <rossellatagliaboschi@libero.it>
2024-07-01 11:34:02 +0200

    in progress

--- src/main/java/com/gol/javahome/controller/AppController.java
+++ src/main/java/com/gol/javahome/controller/AppController.java
@@ -1,6 +1,23 @@
 package com.gol.javahome.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
 public class AppController {
 
+    // prodotti divisi per ambiente casa
+    @GetMapping("/prodotti")
+    public String prodotti() {
+        return "prodotti.html";
+    }
 
+    // redirect Utente per aggiungere prodotti/clienti+ lista clienti
+    @GetMapping("/nuovo")
+    public String nuovo() {
+        return "nuovo.html";
+    }
 }
