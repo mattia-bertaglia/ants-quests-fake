b3983cd7ec150a74930839e03980b2b8b700d721
Vlad-ino <mistintheforest@outlook.com>
2024-07-01 12:58:26 +0200

    sistemato app controller

--- src/main/java/com/gol/javahome/controller/AppController.java
+++ src/main/java/com/gol/javahome/controller/AppController.java
@@ -1,7 +1,6 @@
 package com.gol.javahome.controller;
 
 import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
